96e89b1a05e666ffd70dfc9e80923b49ba88f67c
rogerywoo <rogerywoo@gmail.com>
2020-08-21 12:20:59 -0400

    add more comments

--- contacts/db/config.js
+++ contacts/db/config.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://contacts:contacts@localhost:5432/contacts';
 const { Pool } = require('pg');
 
+console.log(`connect ${DB_CONFIG}`);
+
 const pgconn = new Pool({
     connectionString: DB_CONFIG,
     ssl: false,
